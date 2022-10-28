f507ac1a97e449d6ee4a3434eb2d855f261bf138
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-28 18:59:51 +0200

    Simple data fetched and shown on the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await fetch("http://localhost:15000/users");
+        response.ok ? console.log("Successful") : console.log("failed");
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    loadData();
+  }, []);
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Users : {users.length}</h1>
+      <div>
+        {users.map((user) => (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
