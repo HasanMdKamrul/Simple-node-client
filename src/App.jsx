c0465a621500e40ace8629f19703455536e1a926
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-28 23:44:07 +0200

    getting the users from the server and mongoDb database

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,53 @@ function App() {
     loadData();
   }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+
+    const sendData = async () => {
+      try {
+        const response = await fetch(`http://localhost:15000/users`, {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(user),
+        });
+        response.ok
+          ? console.log("data send Successful")
+          : console.log("data send failed");
+        const data = await response.json();
+        setUsers([...users, data]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    sendData();
+  };
+
   return (
     <div className="App">
-      <h1>Users : {users.length}</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="name" name="name" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
       <div>
-        {users.map((user) => (
-          <div key={user.id}>
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-          </div>
-        ))}
+        <h1>Users : {users.length}</h1>
+        <div>
+          {users.map((user) => (
+            <div key={user._id}>
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
